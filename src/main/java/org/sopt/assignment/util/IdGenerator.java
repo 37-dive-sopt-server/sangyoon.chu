2c235986ec67c575a5815d64c429ada303fe111b
oOccasio <dhzktldh@gmail.com>
2025-10-27 19:48:59 +0900

    feat: FileMemberRepository add

--- src/main/java/org/sopt/assignment/util/IdGenerator.java
+++ src/main/java/org/sopt/assignment/util/IdGenerator.java
@@ -8,6 +8,11 @@ public class IdGenerator {
 
     private static final AtomicLong memberSequence = new AtomicLong(1L);
 
+    public static void syncMemberSequence(Long maxId) {
+        if (maxId != null && maxId >= memberSequence.get()) {
+            memberSequence.set(maxId + 1);
+        }
+    }
     public static Long generateMemberId() {
         return memberSequence.getAndIncrement();
     }
